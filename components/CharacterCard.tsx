d5f6e94f0ce8cd7c6ec1132ca0a0a15439e4d034
Jeimmy López Vega <89615089+Jeim05@users.noreply.github.com>
2024-08-29 15:48:53 -0600

    Updated Card

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -2,22 +2,23 @@ import { Character } from '@/types'
 import Image from 'next/image';
 import React from 'react'
 
-interface CharacterCardProps{
-    character: Character;
+
+interface CharacterCardProps {
+  character: Character;
 }
 
-const CharacterCard = () => {
+const CharacterCard = ({ character }: CharacterCardProps) => {
   return (
-    <div className='flex flex-col items-center bg-blue border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl '>
-        <Image src="/" width={200} height={200} alt='Character image' className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'/>
-        <div className="flex flex-col justify-between p-4 leading-normal">
-        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Name</h2>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Status</p>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"><span classname="font-bold">Ultima ubicación:</span></span>  Ubicación </p>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"><span classname="font-bold">Visto por primera vez en: </span></span> </p>
-        
-    </div>
-
+    <div className='flex flex-col items-center bg-gray-300 border border-gray-200 rounded-lg shadow lg:flex-row md:max-w-xl py-6 px-4 md:py-2 mx-4 my-6'>
+      <Image src={character.image} width={220} height={220} alt={character.name} className='object-cover w-full rounded-t-lg h-48 md:h-auto md:w-full lg:w-48 md:rounded-none md:rounded-s-lg' />
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{character.name}</h2>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{character.status} - {character.species}</p>
+        <p className="mb-0 font-normal text-gray-700 dark:text-gray-400"><span className="font-bold">Ultima ubicación:</span></p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{character.origin.name} </p>
+        <p className="mb-0 font-normal text-gray-700 dark:text-gray-400"><span className="font-bold">Visto por primera vez en: </span> </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{character.location.name} </p>
+      </div>
     </div>
   )
 }
