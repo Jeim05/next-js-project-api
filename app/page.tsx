4a445f6442bdbc79e10b35a8aed118ebaf3dfc63
Jeimmy López Vega <89615089+Jeim05@users.noreply.github.com>
2024-08-29 15:49:29 -0600

    Implementes api

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,36 @@
-import Image from "next/image";
+'use client'
 import CharacterCard from "@/components/CharacterCard";
+import { fetchCharacters } from "@/utils";
+import { useEffect, useState } from "react";
+import { Character } from '../types';
 
-export default function Home() {
+export default function Home({}) {
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  useEffect(()=>{
+    const loadCharacters = async () =>{
+      const data = await fetchCharacters();
+      setCharacters(data);
+    };
+
+    loadCharacters();
+  },[])
+
   return (
-    <main className="min-h-screen flex flex-col items-center p-24">
+    <main className="min-h-screen flex flex-col items-center p-6 lg:p-24">
       <section className="container mx-auto my-8 bg-white">
-        <div className="row-span-1 text-center">
-          <h1 className="text-6xl font-bold ">The Rick and Morthy API</h1>
+        <div className="row-span-1 text-center my-4">
+          <h1 className="text-6xl font-bold underline decoration-solid">The Rick and Morthy API</h1>
         </div>
       </section>
       <section className="max-w-full mx-auto">
-      <div className="grid grid-cols-2 bg-slate-400">
-        <CharacterCard />
-        <CharacterCard />
-      </div>
+        <div className="grid md:grid-cols-2">
+          {
+            characters.map((character) => (
+              <CharacterCard key={character.id} character={character} />
+            ))
+          }
+        </div>
       </section>
     </main>
   );
